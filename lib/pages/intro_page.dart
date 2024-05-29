9d8dfe4c351ebed9e177bf38c54b7948ec522199
Cjoshd <cjoshuadev@gmail.com>
2024-05-29 13:47:19 +0800

    created the intro page with static icons, text and begin container, => convert container to button and make floating icons and blend colours for a more dynamic look in the future

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,4 @@
+import 'package:darth_runner/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatelessWidget {
@@ -6,15 +7,64 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 63, 69, 72),
-      body: Column(
-        children: [
-          //logo
-          Image.asset('lib/assets/images/darthrunner_logo.jpeg')
-          //title
+      backgroundColor: const Color.fromARGB(255, 7, 1, 28),
+      body: Center(
+        child: Column(
+          children: [
+            //logo
+            Padding(
+              padding: const EdgeInsets.only(left:50,top:200,right:50),
+              child: Image.asset(
+                  'lib/assets/images/darthrunner_logo.jpeg',
+                ),
+            ),
+            const SizedBox(height: 40),
 
-          // start button
-        ],
+            // Welcome Text
+            const Text('Welcome to Darth Runner', 
+            textAlign:TextAlign.center,
+            style: TextStyle(
+              color: Color.fromARGB(202, 255, 255, 255),
+              fontWeight: FontWeight.bold,
+              fontSize: 30
+              ),
+            ),
+            const SizedBox(height: 10,),
+
+            // Start button
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>  const HomePage()
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(40),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color:  Color.fromARGB(255, 38, 15, 77),
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: const Center(
+                    child: Text('Begin My Journey',
+                     textAlign: TextAlign.center,
+                     style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20,
+                
+                        ),
+                     )
+                  )
+                ),
+              ),
+            )
+
+
+          ],
+        ),
       )
       );
   }
