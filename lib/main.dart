cddb20872604e4d19e76ee55e283aa0e16221d53
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-05-31 15:28:55 +0800

    updated min sdk version and remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter',
       home: LoginScreen(),
     );
