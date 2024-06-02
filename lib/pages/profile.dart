17a2a59f92721ec00bd523f74a98af87549f930a
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-06-02 16:57:49 +0800

    debugged not signout properly

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,6 @@
+import 'package:darth_runner/auth/auth_service.dart';
+import 'package:darth_runner/auth/login_screen.dart';
+import 'package:darth_runner/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
@@ -5,41 +8,46 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final auth = AuthService();
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration:const BoxDecoration(
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            // begin: Alignment(-1,-1),
-            // end: Alignment(0.7,1),
-            colors: [
-              Color.fromARGB(255, 6, 4,120),
-               Color.fromARGB(255, 174, 12,0)],
-              ),
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              // begin: Alignment(-1,-1),
+              // end: Alignment(0.7,1),
+              colors: [
+                Color.fromARGB(255, 6, 4, 120),
+                Color.fromARGB(255, 174, 12, 0)
+              ],
             ),
           ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child:
-              AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                title: const Text('Your Profile',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-
-              ) 
+        ),
+        Column(
+          children: [
+            AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: const Text(
+                'Your Profile',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              label: "Sign Out",
+              onPressed: () async {
+                await auth.signout();
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => const LoginScreen()));
+              },
             )
-        ],        
-      )
-    );
+          ],
+        )
+      ],
+    ));
   }
 }
