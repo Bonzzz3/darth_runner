cebf20b700bdb44ac402477da70b567ea8bcd04e
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-06-04 15:02:04 +0800

    Fixed signout button with phoenix package

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,9 +1,10 @@
 import 'package:darth_runner/auth/auth_service.dart';
-//import 'package:darth_runner/auth/login_screen.dart';
-// import 'package:darth_runner/home_screen.dart';
 import 'package:darth_runner/widgets/button.dart';
 import 'package:flutter/material.dart';
-import 'package:restart_app/restart_app.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+//import 'package:restart_app/restart_app.dart';
+//import 'package:darth_runner/auth/login_screen.dart';
+// import 'package:darth_runner/home_screen.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -44,7 +45,8 @@ class Profile extends StatelessWidget {
               label: "Sign Out",
               onPressed: () async {
                 await auth.signout();
-                Restart.restartApp();
+                Phoenix.rebirth(context);
+                //Restart.restartApp();
                 // Navigator.push(
                 //   context,
                 //   MaterialPageRoute(builder: (context) => const LoginScreen()),
