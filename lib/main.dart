ee1468e7eb2e8e15efefa011009d8c61e0c2a42c
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-06-04 15:07:08 +0800

    Fixed signout feature using phoenix package

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'dart:async';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 // import 'package:darth_runner/pages/intro_page.dart';
 // import 'package:flutter/services.dart';
 
-
 Future<void> main() async {
- // enable the app to be in full screen, hiding the notification bar
+  // enable the app to be in full screen, hiding the notification bar
 // uncomment the below and package import
-// problems when loading screen, need to add padding around appbar 
+// problems when loading screen, need to add padding around appbar
 // the bottom nav bar end up hiding
 
   // WidgetsFlutterBinding.ensureInitialized();
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    Phoenix(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -25,13 +29,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        theme: (
-        ThemeData(
-          primarySwatch: Colors.blue
-        )
-      ),
+      theme: (ThemeData(primarySwatch: Colors.blue)),
       debugShowCheckedModeBanner: false,
       home: const Wrapper(),
       //wrapper direct to homepage which is IntroPage()
