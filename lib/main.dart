e24195bdb50a7d7cb8bde9530b5afc1832892c58
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-07-02 13:27:23 +0800

    fixed ios not starting app due to old deleted firebase file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:darth_runner/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,22 +6,27 @@ import 'firebase_options.dart';
 import 'dart:async';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'dart:developer';
 import 'package:hive_flutter/hive_flutter.dart';
 
 // import 'package:darth_runner/pages/intro_page.dart';
 // import 'package:flutter/services.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Firebase.initializeApp();
+  if (Platform.isAndroid) {
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
+      } else if (Platform.isIOS) {
+        await Firebase.initializeApp();
+      }
 
   //CREATING BOX
   var box = await Hive.openBox('myRuns');
-  
+
   runApp(
     Phoenix(
       child: const MyApp(),
@@ -34,7 +40,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       theme: (ThemeData(primarySwatch: Colors.blue)),
       debugShowCheckedModeBanner: false,
       home: const Wrapper(),
